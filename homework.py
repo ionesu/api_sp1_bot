18068a5fdf654ecd4e576ef2ce2e6b27ddcea945
Ivan Sushkov <ivan.sushkov91@gmail.com>
2020-12-11 10:50:12 +0200

    Add: homework

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import time
 
@@ -7,47 +8,76 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
 
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+url_praktikum = 'https://praktikum.yandex.ru/api/user_api/'
+delay_get = 300
+delay_error = 5
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
-        verdict = 'К сожалению в работе нашлись ошибки.'
+    homework_name = homework.get('homework_name')
+    answer = {'rejected': 'К сожалению в работе нашлись ошибки.',
+              'approved': ('Ревьюеру всё понравилось, можно '
+                           'приступать к следующему уроку.')
+              }
+    homework_status = homework.get('status')
+    if homework_status in ('rejected', 'approved'):
+        verdict = answer[homework_status]
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        logging.error(
+            '\tInvalid response from the server. The server responded "{}".'.format(
+                homework_status))
+        return 'Wrong response from the server.'
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    if current_timestamp is None:
+        current_timestamp = int(time.time())
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    url = '{}{}'.format(url_praktikum, 'homework_statuses/')
+    try:
+        homework_statuses = requests.get(url,
+                                         params={
+                                             'from_date': current_timestamp},
+                                         headers=headers)
+    except requests.exceptions.Timeout:
+        logging.error("\tVery Slow Internet Connection.")
+        return {}
+    except requests.exceptions.ConnectionError:
+        logging.error("\tNetwork Unavailable. Check your connection.")
+        return {}
+    except requests.exceptions.MissingSchema:
+        logging.error("\t503 Service Unavailable. Retrying download ... ")
+        return {}
     return homework_statuses.json()
 
 
-def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+def send_message(message):
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    # проинициализировать бота здесь
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = int(time.time())
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+                send_message(
+                    parse_homework_status(new_homework.get('homeworks')[0]))
+            current_timestamp = new_homework.get('current_date')
+            time.sleep(delay_get)
 
         except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
-            time.sleep(5)
+            print(f'Bot die with exception: {e}')
+            time.sleep(delay_error)
+            continue
 
 
 if __name__ == '__main__':
